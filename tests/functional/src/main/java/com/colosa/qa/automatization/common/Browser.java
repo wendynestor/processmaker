fe8991342947a8166d64e552500726950bf545d6
Herbert Saal <herbert.saal@colosa.com>
2012-11-13 11:09:41 -0400

    Add support for Selenium Grid
    Create a RemoteDriver in the case that the configuration is set as remote.

--- tests/functional/src/main/java/com/colosa/qa/automatization/common/Browser.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/common/Browser.java
@@ -1,6 +1,5 @@
 package com.colosa.qa.automatization.common;
 
-//import org.openqa.selenium.IWebDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +7,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,10 +20,58 @@ public class Browser {
 	private static WebDriver _driver = null;
 
 	public static WebDriver driver() throws FileNotFoundException, IOException{
-		if(_driver == null)
-			_driver = (ConfigurationSettings.getInstance().getSetting("browser.name").equals("chrome"))?new ChromeDriver():
-					((ConfigurationSettings.getInstance().getSetting("browser.name").equals("ie"))?new InternetExplorerDriver():
+		if(_driver == null){
+			String browserMode = ConfigurationSettings.getInstance().getSetting("browser.mode");
+			String browserName = "firefox";
+
+			browserName = ConfigurationSettings.getInstance().getSetting("browser.name");
+
+			if(browserMode.equals("local")){
+				_driver = (browserName.equals("chrome"))?new ChromeDriver():
+					((browserName.equals("ie"))?new InternetExplorerDriver():
 						new FirefoxDriver());
+			}
+
+			if(browserMode.equals("remote")){
+				String browserVersion = ConfigurationSettings.getInstance().getSetting("browser.version");
+				String browserPlatform = ConfigurationSettings.getInstance().getSetting("browser.platform");
+				String remoteServerUrl = ConfigurationSettings.getInstance().getSetting("remote.server.url");
+
+				System.out.printf("Remote browser:%s, version:%s, platform:%s, url:%s \n", 
+					browserName, browserVersion, browserPlatform, remoteServerUrl); 
+
+				DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+				if(browserName.equals("chrome")){
+					desiredCapabilities = DesiredCapabilities.chrome();				
+				}else if(browserName.equals("internet explorer")){
+					desiredCapabilities = DesiredCapabilities.internetExplorer();					
+				}else if(browserName.equals("firefox")){
+					desiredCapabilities = DesiredCapabilities.firefox();					
+				}
+
+				if(browserVersion != null && !browserVersion.equals("")){
+					desiredCapabilities.setVersion(browserVersion);
+				}
+
+				if(browserPlatform != null && !browserPlatform.equals("")){
+					if(browserPlatform.equals("WINDOWS")){
+						desiredCapabilities.setPlatform(Platform.WINDOWS);				
+					}else if(browserPlatform.equals("LINUX")){
+						desiredCapabilities.setPlatform(Platform.LINUX);					
+					}else if(browserPlatform.equals("MAC")){
+						desiredCapabilities.setPlatform(Platform.MAC);					
+					}
+				}else{
+					desiredCapabilities.setPlatform(Platform.ANY);
+				}
+
+				URL url=new URL(remoteServerUrl);
+
+				_driver = new RemoteWebDriver(url, desiredCapabilities);
+
+			}
+		}
 		return _driver;
 	}
 	//private String cs = ConfigurationSettings.getInstance().getSetting("browse.name");
